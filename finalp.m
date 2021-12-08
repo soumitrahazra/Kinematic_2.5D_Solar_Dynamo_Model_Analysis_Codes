00851d736de24085f818800f6fd05b0294c1ceaa
Soumitra Hazra <31670316+soumitrahazra@users.noreply.github.com>
2021-12-08 10:41:23 -0500

    Add files via upload
    Poloidal field evolution

---
+++ finalp.m
@@ -0,0 +1,12 @@
+ load final.dat;
+ a=final;
+ n=length(a);
+ R=a(1:300:n,2);
+ T=a(1:300,1);
+ [r,t]=meshgrid(R,T);
+ x=r.*sin(t);
+ y=r.*cos(t);
+ f=reshape(a(:,3),300,n/300);
+ contourf(x,y,f,20); shading flat
+ colorbar;
+ strip;
