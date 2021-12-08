f23cb16c2c4382ce0211da7c1ffbd3a6184fab09
Soumitra Hazra <31670316+soumitrahazra@users.noreply.github.com>
2021-12-08 11:47:03 -0500

    Add files via upload

---
+++ parity_calculation_CNC_paper.m
@@ -0,0 +1,32 @@
+ load run.dat;
+ load ram.dat;
+ x=run(:,1);
+ y=run(:,2);
+ z=ram(:,2);
+ dt=0.01;
+ p=10^8/(365*24*3600);
+ nt=round(5.5/(p*dt));
+ s=1
+ for n=175:350:(length(x)-2*nt)
+  int1=0;
+  int2=0;
+  int3=0;
+ for k=n-nt:n+nt
+ int1=int1+ y(k)*z(k);
+ end
+ for k=n-nt:n+nt
+ int2=int2+y(k)*y(k);
+ end
+ for k= n-nt:n+nt
+ int3=int3+ z(k)*z(k);
+ end
+
+ pa(s)= int1/(sqrt(int2)*sqrt(int3));
+  t(s)=n*p*dt;
+  s=s+1;
+  end
+
+  m=[t;pa];
+ fid=fopen('paarit.dat','w');
+ fprintf(fid, '%6.8f  %6.8f\n',m);
+ fclose(fid);
