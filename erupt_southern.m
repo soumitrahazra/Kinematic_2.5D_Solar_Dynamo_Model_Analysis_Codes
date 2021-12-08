e147f1fc6b0cc64366ee0a85c449da755f453547
Soumitra Hazra <31670316+soumitrahazra@users.noreply.github.com>
2021-12-08 11:33:12 -0500

    Add files via upload

---
+++ erupt_southern.m
@@ -0,0 +1,52 @@
+load double2.dat
+x=double2(:,1);
+np=length(x);
+p=10^8/(365*24*3600);
+t=x.*p;
+a=1;
+s=2;
+z=2;
+n(1)=0;
+m(1)=0;
+y(1)=0;
+for i=2:np
+  if (t(i) > a)
+ n(z)=i;
+ y(s)=t(i);
+ m(s)=i-n(z-1);
+ s=s+1;
+ z=z+1;
+ a=a+1;
+end
+end
+ v=[y;m];
+ fid=fopen('erupt_souths.dat', 'w');
+ fprintf(fid, '%6.8f  %6.8f\n',v);
+ fclose(fid);
+
+ k=1;
+for i=1:length(y);
+if (m(i)>1)
+ zp(k)=y(i);
+ mp(k)=m(i);
+ k=k+1;
+end
+end
+f=floor(zp);
+nx=length(f);
+l=1;
+ for i=1:f(nx);
+ if (i==f(l));
+jp(i)=f(l);
+hp(i)=mp(l);
+l=l+1;
+else
+jp(i)=i;
+hp(i)=0;
+end
+end
+vp=[jp;hp];
+
+fid=fopen('erupt_southsss.dat', 'w');
+ fprintf(fid, '%6.8f  %6.8f\n',vp);
+ fclose(fid);
