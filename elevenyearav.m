0f41d592d9ea4620748aa94d433dd982d6993b75
Soumitra Hazra <31670316+soumitrahazra@users.noreply.github.com>
2021-12-08 14:20:46 -0500

    Add files via upload

---
+++ elevenyearav.m
@@ -0,0 +1,52 @@
+ load erupt_northsss.dat;
+ mydata = erupt_northsss; 
+ f=1;
+ store = [] ;
+ while (f<4115)
+    
+  sum =0 ;   
+    for i=1:11
+    sum = sum + mydata(f,2);
+    f=f+1;
+    end
+    store = [ store sum ];
+  f;
+ end
+
+ store_north = store ; 
+ load erupt_southsss.dat ; 
+ mydata2 = erupt_southsss ; 
+ f=1;
+ store_south = [] ;
+ while (f<4115)
+    
+  sum =0 ;   
+    for i=1:11
+    sum = sum + mydata2(f,2);
+    f=f+1;
+    end
+    store_south = [ store_south sum ];
+    f;
+ end
+ %plot(store_north)
+ %hold on ;
+ %plot(store_south,'r') ;
+ diff = store_north - store_south ;
+ diffn = diff./max(diff);
+ %figure ;
+  x=[];
+ for i=12:11:4120;
+ x=[ x i] ;
+ end
+ %plot(x,store_north)
+ %plot(x,diffn);
+
+ load paarit.dat ;
+ figure ; plot(x,diffn,'-b*','linewidth',2,'markersize',10);
+ %hold on
+ %plot(x,diffn,'*b','markersize',12);
+ hold on ;
+ plot(paarit(:,1),paarit(:,2),'-r','linewidth',2) 
+ xlabel('Time (years)','fontsize',14)
+ legend('Signed Asymmetry','Parity')
+ set(legend,'fontSize',14)
